5dba2e58b5ae35f53552179eb921deb7ba4b0954
stdiff <stdiff@outlook.de>
2024-03-10 01:33:07 +0100

    use dvc to read data sets

--- hotels/load_data.py
+++ hotels/load_data.py
@@ -3,28 +3,34 @@ The purpose of this module is to provide functions to load various data sets
 """
 
 import pandas as pd
+import dvc.api as dvc
 
 from hotels import DATA_DIR
 
-COUNTRY_CODE_PATH = DATA_DIR / "country_code.csv"
 hotel_raw_data_path = DATA_DIR / "raw" / "hotels.parquet"
 bookings_data_path = DATA_DIR / "cleaned" / "bookings.parquet"
 action_data_path = DATA_DIR / "aggregated" / "action.parquet"
 
+fs = dvc.DVCFileSystem(rev="main")
+
 
 def load_raw_hotel_data() -> pd.DataFrame:
-    return pd.read_parquet(hotel_raw_data_path)
+    with fs.open("/data/raw/hotels.parquet") as fo:
+        return pd.read_parquet(fo)
 
 
 def load_country_code_mapping() -> dict[str, str]:
     """Mapping table between country code (such as JPN) and country name (Japan)"""
-    df = pd.read_csv(COUNTRY_CODE_PATH)
+    with fs.open("/data/country_code.csv") as fo:
+        df = pd.read_csv(fo)
     return df.set_index("code")["country"].to_dict()
 
 
 def load_booking_data() -> pd.DataFrame:
-    return pd.read_parquet(bookings_data_path)
+    with fs.open("/data/cleaned/bookings.parquet") as fo:
+        return pd.read_parquet(fo)
 
 
 def load_action_data() -> pd.DataFrame:
-    return pd.read_parquet(action_data_path)
+    with fs.open("/data/aggregated/action.parquet") as fo:
+        return pd.read_parquet(fo)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,15 @@
+import os
+
+import pandas as pd
 import streamlit as st
+from dvc import api as dvc
 
 from hotels import PROJ_ROOT
 
 readme_path = PROJ_ROOT / "README.md"
+os.environ["AWS_ACCESS_KEY_ID"] = st.secrets["aws"]["AWS_ACCESS_KEY_ID"]
+os.environ["AWS_SECRET_ACCESS_KEY"] = st.secrets["aws"]["AWS_SECRET_ACCESS_KEY"]
+
 
 if __name__ == "__main__":
     with readme_path.open() as fo:
